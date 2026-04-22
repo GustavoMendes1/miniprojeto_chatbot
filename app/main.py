c602fbc9e3c4f696015c089584c806ff388c2b51
Gustavo Mendes <gustavoomendesoliveira@gmail.com>
2026-04-21 23:08:50 -0300

    feat: adicionando novas rotas

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.api.routes import upload
+from app.api.routes import chat
 
 app = FastAPI(title="RAG API")
 
 app.include_router(upload.router, prefix="/api")
+app.include_router(chat.router, prefix="/api")
 
 @app.get("/")
 def root():
